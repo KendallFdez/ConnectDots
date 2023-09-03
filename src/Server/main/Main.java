d40d683c17058b04c11df2dca2be82dedae5fe84
melivasr <anavasquez@estudiantec.cr>
2023-09-03 12:19:27 -0600

    Carpetas 2.0

--- src/Server/main/Main.java
+++ src/Server/main/Main.java
@@ -11,5 +11,19 @@ public class Main {
         micola.enqueue(40);
         System.out.println(micola.dequeue().getData()+" se elimino de la cola");
         System.out.println("El tamaño de la cola es "+micola.getSize());
+
+        ListaDoble lista = new ListaDoble();
+        lista.insertLast(1);
+        lista.insertLast(10);
+        lista.insertLast(15);
+        lista.insertLast(20);
+        lista.insertLast(30);
+        lista.insertLast(40);
+        lista.insertFirst(0);
+        lista.deleteFirst();
+        lista.deleteLast();
+        lista.displayForward();
+        int[] a = new int[8];
+
     }
 }
